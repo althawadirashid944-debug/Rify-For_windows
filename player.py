f8b47cf6e4efa84f12d22e72b491eb0a539c03ad
althawadirashid944-debug <althawadirashid944@gmail.com>
2026-07-14 17:36:57 +0300

    fixing

--- player.py
+++ player.py
@@ -1,9 +1,24 @@
+from pathlib import Path
+import sys
+
+
 import mpv
 
+def get_resource_path(relative):
+    if getattr(sys, "frozen", False):
+        base = Path(sys.executable).parent
+    else:
+        base = Path(__file__).parent
+    return base / relative
 class Player:
     def __init__(self):
-        self.player = mpv.MPV()
-        self.current_song = None
+        mpv_exe = get_resource_path(
+            Path("mpv") / "mpv.exe"
+        ) 
+        self.player = mpv.MPV(
+            executables=str(mpv_exe)
+        )
+       
 
     def play(self, path):
         self.current_song = path
